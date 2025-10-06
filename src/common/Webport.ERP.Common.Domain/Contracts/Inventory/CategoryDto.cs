e76e3562c48668c5b6b929de8a509f0b37f0f94b
Saif <Saif@SAIF-PC2>
2025-10-06 14:11:00 +0200

    some changes

--- src/common/Webport.ERP.Common.Domain/Contracts/Inventory/CategoryDto.cs
+++ src/common/Webport.ERP.Common.Domain/Contracts/Inventory/CategoryDto.cs
@@ -1,8 +1,14 @@
-﻿namespace Webport.ERP.Common.Domain.Contracts.Inventory;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webport.ERP.Common.Domain.Contracts.Inventory;
 
 public class CategoryDto
 {
     public int CategoryId { get; set; }
+
+    [Required(ErrorMessage = "sssss")]
     public string CategoryCode { get; set; } = string.Empty;
+
+    [Required]
     public string CategoryDesc { get; set; } = string.Empty;
 }
